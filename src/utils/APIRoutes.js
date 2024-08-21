7dadfac27c4fd21fa8db1328818b4a4b0a590ef8
hoangvietann <hoangan29102003@gmail.com>
2024-08-21 09:50:38 +0700

    demo1

--- src/utils/APIRoutes.js
+++ src/utils/APIRoutes.js
@@ -1,4 +1,4 @@
-export const host = "https://server-chat-7mki.onrender.com";
+export const host = "https://demo-sv-1tce.onrender.com";
 export const loginRoute = `${host}/api/auth/login`;
 export const registerRoute = `${host}/api/auth/register`;
 export const logoutRoute = `${host}/api/auth/logout`;
